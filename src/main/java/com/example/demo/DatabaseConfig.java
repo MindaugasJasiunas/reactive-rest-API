ac23981a92d72de9d70e24c77762acedd09676c2
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-07-13 13:29:52 +0300

    Manually registered data.sql too. #2

--- src/main/java/com/example/demo/DatabaseConfig.java
+++ src/main/java/com/example/demo/DatabaseConfig.java
@@ -1,25 +1,26 @@
 package com.example.demo;
 
 import io.r2dbc.spi.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @Configuration
 public class DatabaseConfig {
 
-    @Value("classpath:/schema.sql")  //load that resource
-    Resource resource;  //import org.springframework.core.io
-
     @Bean
     ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory){  // connectionFactory created at runtime
         ConnectionFactoryInitializer initializer= new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
-        initializer.setDatabasePopulator(new ResourceDatabasePopulator(resource));
-//        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));  // alternative way
+
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
+        initializer.setDatabasePopulator(populator);
+
         return initializer;
     }
 
