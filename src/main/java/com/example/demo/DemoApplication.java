fe3c5641059f4074115063c00a18c3d427ba2b0b
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-07-25 12:43:24 +0300

    Added dependency & configuration to register itself to Eureka Server. Closes #14

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,9 @@ import com.example.demo.security.jwt.JwtTokenProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableConfigurationProperties({JwtTokenProvider.class})
 @SpringBootApplication
 public class DemoApplication {
