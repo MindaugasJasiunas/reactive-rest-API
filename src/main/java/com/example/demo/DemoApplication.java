e032cb2917f9e9da38e6cd7a8a1096f5a746e548
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-29 15:41:32 +0300

    COURIER role & authority added, initial default users added for testing purposes.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.repository.UserRepository;
 import com.example.demo.security.jwt.JwtTokenProvider;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @EnableDiscoveryClient
 @EnableConfigurationProperties({JwtTokenProvider.class})
@@ -14,5 +23,24 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Autowired
+	UserRepository userRepo;
+//	UserService userService;
+	@Bean
+	CommandLineRunner initDatabase() {
+		return args -> {
+			// initial users
+//			User simpleUser = new User(null, UUID.randomUUID(), "johndoe", "password", "John", "Doe", LocalDateTime.now(), LocalDateTime.now(), 1L, null, true, true, true, true);
+//			userRepo.save(simpleUser).subscribe();
+//
+//			User adminUser = new User(null, UUID.randomUUID(), "admin", "password", "Admin", "Admin", LocalDateTime.now(), LocalDateTime.now(), 3L, null, true, true, true, true);
+//			userRepo.save(adminUser).subscribe();
+//
+//			User courier = new User(null, UUID.randomUUID(), "janedoe", "password", "Jane", "Doe", LocalDateTime.now(), LocalDateTime.now(), 4L, null, true, true, true, true);
+//			userRepo.save(courier).subscribe();
+		};
+	}
+
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,15 @@
 INSERT INTO role_entity (id, role_name) VALUES (1, 'ROLE_USER') ON CONFLICT DO NOTHING;
 INSERT INTO role_entity (id, role_name) VALUES (2, 'ROLE_MANAGER') ON CONFLICT DO NOTHING;
 INSERT INTO role_entity (id, role_name) VALUES (3, 'ROLE_ADMIN') ON CONFLICT DO NOTHING;
+INSERT INTO role_entity (id, role_name) VALUES (4, 'ROLE_COURIER') ON CONFLICT DO NOTHING;
 
-ALTER SEQUENCE roles_sequence RESTART WITH 4;
+ALTER SEQUENCE roles_sequence RESTART WITH 5;
 
 INSERT INTO authority (id, authority_name) VALUES (1, 'user:create') ON CONFLICT DO NOTHING;
 INSERT INTO authority (id, authority_name) VALUES (2, 'user:read') ON CONFLICT DO NOTHING;
 INSERT INTO authority (id, authority_name) VALUES (3, 'user:update') ON CONFLICT DO NOTHING;
 INSERT INTO authority (id, authority_name) VALUES (4, 'user:delete') ON CONFLICT DO NOTHING;
+INSERT INTO authority (id, authority_name) VALUES (5, 'courier') ON CONFLICT DO NOTHING;
 
 INSERT INTO role_authority (role_id, authority_id) VALUES (1, 2) ON CONFLICT DO NOTHING;
 
@@ -18,6 +20,8 @@ INSERT INTO role_authority (role_id, authority_id) VALUES (3, 1) ON CONFLICT DO 
 INSERT INTO role_authority (role_id, authority_id) VALUES (3, 2) ON CONFLICT DO NOTHING;
 INSERT INTO role_authority (role_id, authority_id) VALUES (3, 3) ON CONFLICT DO NOTHING;
 INSERT INTO role_authority (role_id, authority_id) VALUES (3, 4) ON CONFLICT DO NOTHING;
+
+INSERT INTO role_authority (role_id, authority_id) VALUES (4, 5) ON CONFLICT DO NOTHING;
 
 -- INSERT INTO user_entity (id, public_id, username, password, first_name, last_name, role_id, created_date, last_modified_date, is_account_non_expired, is_account_non_locked, is_credentials_non_expired, is_enabled)
 -- VALUES (1, 'd51f1234-3d7d-4100-8846-468f38e14a4f', 'johnd', '$2a$12$L61SNM2qG1YPyD4.bG02OOUBO.oW8QOT51CwMlgQ/7HibB8bhXXuO', 'John', 'Doe', 1, '2022-07-13 12:00:00.000000', '2022-07-13 12:00:00.000000', true, true, true, true) ON CONFLICT DO NOTHING;
